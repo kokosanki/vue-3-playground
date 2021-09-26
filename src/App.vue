db218e96081f4cc52baa5e38977d322b6cf35d58
kokosanki <os_mo@wp.pl>
2021-09-26 19:26:03 +0200

    add teleport example

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@
 </template>
 
 <style lang="scss">
+body {
+  position: relative;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <teleport-modal />
   </div>
 </template>
+<script>
+import TeleportModal from '../components/Teleport-Modal.vue'
+export default {
+  components: {
+    TeleportModal
+  }
+}
+</script>
