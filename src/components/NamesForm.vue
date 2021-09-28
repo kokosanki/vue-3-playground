1bd910217fcbfd46a9a40276d5f6d98419917ed1
kokosanki <os_mo@wp.pl>
2021-09-28 21:21:24 +0200

    many-v-models example

---
+++ src/components/NamesForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form>
+    <div>
+      <label for="name">Author:</label>
+      <input type="text" :value="property" @input="updateProperty($event.target.value)" />
+    </div>
+    <div>
+      <label for="Recipient">Recipient:</label>
+      <input type="text" :value="recipient" @input="updateRecipient($event.target.value)" />
+    </div>
+  </form>
+</template>
+<script>
+export default {
+  props: {
+    property: String,
+    recipient: String
+  },
+  setup (props, { emit }) {
+    const updateProperty = (value) => {
+      emit('update:property', value)
+    }
+    const updateRecipient = (value) => {
+      emit('update:recipient', value)
+    }
+    return { updateProperty, updateRecipient }
+  }
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,13 +2,26 @@
   <div class="about">
     <h1>This is an about page</h1>
     <teleport-modal />
+    <h2>Look {{recipientName}}! I have {{authorProperty}}!</h2>
+    <names-form v-model:property="authorProperty" v-model:recipient="recipientName" />
   </div>
 </template>
 <script>
+import { ref } from '@vue/reactivity'
+import NamesForm from '../components/NamesForm.vue'
 import TeleportModal from '../components/Teleport-Modal.vue'
 export default {
   components: {
-    TeleportModal
+    TeleportModal,
+    NamesForm
+  },
+  setup () {
+    const authorProperty = ref('2 v-models')
+    const recipientName = ref('Ma')
+    return {
+      authorProperty,
+      recipientName
+    }
   }
 }
 </script>
